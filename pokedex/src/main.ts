3431940173c48a50d68818077d04ce65d6a81ab3
René <diaz.de.sandi.7@gmail.com>
2023-09-18 20:10:56 -0700

    MongoDB

--- pokedex/src/main.ts
+++ pokedex/src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Agrega prefijo global de "pokemon" a "api/pokemon"
   app.setGlobalPrefix('api/v2');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
   await app.listen(3000);
 }
