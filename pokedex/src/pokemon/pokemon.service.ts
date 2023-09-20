8891f55fcb88722463bc6b7f6ca5d303d181a70e
René <diaz.de.sandi.7@gmail.com>
2023-09-19 20:57:47 -0700

    Pagination

--- pokedex/src/pokemon/pokemon.service.ts
+++ pokedex/src/pokemon/pokemon.service.ts
@@ -9,6 +9,7 @@ import { Pokemon } from './entities/pokemon.entity';
 
 import { CreatePokemonDto, UpdatePokemonDto } from './dto';
 import { InjectModel } from '@nestjs/mongoose';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
 
 @Injectable()
 export class PokemonService {
@@ -28,8 +29,12 @@ export class PokemonService {
     }
   }
 
-  findAll() {
-    return `This action returns all pokemon`;
+  findAll(paginationDto: PaginationDto) {
+    const { limit = 10, offset = 0 } = paginationDto;
+
+    return this.pokemonModel.find().limit(limit).skip(offset).sort({
+      no: 1, // Ordenar de forma ascendente
+    });
   }
 
   async findOne(term: string) {
