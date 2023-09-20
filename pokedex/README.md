c81206cda1fae1501bf02e6d21a7ca70b379f08a
René <diaz.de.sandi.7@gmail.com>
2023-09-19 20:22:37 -0700

    Seed Populate

--- pokedex/README.md
+++ pokedex/README.md
@@ -13,6 +13,11 @@ npm i -g @nestjs/cli
 ```
 docker-compose up -d
 ```
+5. Rebuild database
+```
+http://localhost:3000/api/v2/seed
+```
+
 ## Stack
 * Mongo DB
 * NestJS
--- pokedex/src/seed/seed.service.ts
+++ pokedex/src/seed/seed.service.ts
@@ -1,8 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class SeedService {
-  executeSeed() {
+  private readonly axios: AxiosInstance = axios;
+
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
+
+  async executeSeed() {
+    await this.pokemonModel.deleteMany(); // DELETE * FROM pokemons
+
+    const { data } = await this.axios.get<PokeResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=650',
+    );
+
+    const pokemonToInsert: { name: string; no: number }[] = [];
+
+    const pokemons = data.results.map(({ name, url }) => ({
+      name,
+      no: +url.split('/')[6],
+    }));
+
+    await this.pokemonModel.insertMany(pokemons);
+
     return 'Seed executed';
   }
 }
