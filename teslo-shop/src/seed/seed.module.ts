ac4faca4a514a086f7e80f6a6322710275c534d7
René <diaz.de.sandi.7@gmail.com>
2023-10-04 17:31:42 -0700

    path fix

--- teslo-shop/src/seed/seed.module.ts
+++ teslo-shop/src/seed/seed.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
 import { ProductsModule } from '../products/products.module';
-import { AuthModule } from 'src/auth/auth.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   controllers: [SeedController],
